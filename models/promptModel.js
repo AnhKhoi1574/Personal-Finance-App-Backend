481913df0a0b7a9f66da6a71a15cb856fd54948f
AnhKhoi1574 <s3980639@rmit.edu.vn>
2024-07-31 20:30:06 +0700

    create prompt model

---
+++ models/promptModel.js
@@ -0,0 +1,43 @@
+// import
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+// Prompt schema
+const promptSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    enum: ['user', 'ai'],
+    required: true,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 250,
+    trim: true,
+  },
+  createdDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+// Prompt model
+const Prompt = mongoose.model('Prompt', promptSchema);
+
+// Joi validated function
+function validatePrompt(prompt) {
+  const schema = Joi.object({
+    role: Joi.string().valid('user', 'ai').required(),
+    content: Joi.string().min(3).max(250).required(),
+    createdDate: Joi.date(),
+  });
+  return Joi.validate(prompt);
+}
+
+// export
+exports.Prompt = Prompt;
+exports.promptSchema = promptSchema;
+exports.validatePrompt = validatePrompt;
